86fda85e29ee403c961acff9b9b472f94db10be8
pedroigosl <pedroigosl@gmail.com>
2022-03-29 18:47:15 -0300

    added more skeleton functions to l298n library

--- arduino nano platform/lib/dcmotor_l298n.h
+++ arduino nano platform/lib/dcmotor_l298n.h
@@ -6,13 +6,38 @@
 class dcMotor
 {
 public:
-    dcMotor(uint8_t motorNum);
-    void run(uint8_t, uint8_t);
+    // Sets a dcMotor and pins for direction
+    dcMotor(uint8_t motor_num, uint8_t pin_a, uint8_t pin_b);
+    // Sets a dcMotor, pins for direction and pin for speed
+    dcMotor(uint8_t motor_num, uint8_t pin_a, uint8_t pin_b, uint8_t pin_s);
+    // If inverted true, inverts motor output
+    dcMotor(uint8_t motor_num, uint8_t pin_a, uint8_t pin_b, bool inverted);
+    // If inverted true, inverts motor output
+    dcMotor(uint8_t motor_num, uint8_t pin_a, uint8_t pin_b, uint8_t pin_s, bool inverted);
+    // sets INa = 1 and INb = 0
+    void runForward();
+    // sets INa = 0 and INb = 1
+    void runBackward();
+    // sets all IN pins to 0
+    void stop();
+    // updates motor number
+    void setMotor(uint8_t motor_num);
+    // sets direction pins for motor
+    void setPins(uint8_t pin_a, uint8_t pin_b);
+    // sets direction and speed pins for motor
+    void setPins(uint8_t pin_a, uint8_t pin_b, uint8_t pin_s);
     void setTurning(uint8_t);
     void setSpeed(uint8_t);
 
 private:
-    uint8_t motorNum;
+    uint8_t motor_num;
+    uint8_t pin_a, pin_b; // direction pins
+    uint8_t pin_s;        // speed pin
+    uint8_t speed = 0;
+    uint8_t pinout_a = 0, pinout_b = 0; // direction pins outputs
+    // TO CHECK. THE PIN IS PWM, SO 0 IS ACTUALLY MOVING BACKWARD
+    uint8_t pinout_s = 0;  // speed pin output
+    bool inverted = false; // whether to change motor direction
 };
 
 #endif
